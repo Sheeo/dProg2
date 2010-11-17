6871e3f0bb7e9d7ce9536cf1d14034580a8c0551
Mathias Rav <rav@cs.au.dk>
2010-11-17 12:41:28 +0100

    [Week 3] Remove unnecessary packages in IconCrafter.java

--- Week 3/IconCrafter.java
+++ Week 3/IconCrafter.java
@@ -1,8 +1,6 @@
-import javax.swing.*;
-import java.awt.event.*;
-import java.util.*;
 import java.awt.*;
-import javax.swing.event.*;
+import java.awt.event.*;
+import javax.swing.*;
 import javax.swing.plaf.basic.*;
 
 public class IconCrafter extends JFrame {
