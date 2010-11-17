e0b20e0da6885dc16e713ccf86d4c59a17d5ab4b
Mathias Rav <rav@cs.au.dk>
2010-11-17 12:43:02 +0100

    [Week 3] Remove unnecessary packages in CompositeIcon.java

--- Week 3/CompositeIcon.java
+++ Week 3/CompositeIcon.java
@@ -1,8 +1,6 @@
-import javax.swing.*;
-import java.awt.event.*;
-import java.util.*;
 import java.awt.*;
-import javax.swing.event.*;
+import java.util.*;
+import javax.swing.*;
 
 public class CompositeIcon implements Icon {
 	private ArrayList<Icon> icons;
