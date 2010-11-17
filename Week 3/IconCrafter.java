74f281320db3a5ed847915eb0e5495ce60091810
Mathias Rav <rav@cs.au.dk>
2010-11-17 12:31:16 +0100

    [Week 3] Make it possible to specify colors on the command-line, and add a README telling you about the defaults

--- Week 3/IconCrafter.java
+++ Week 3/IconCrafter.java
@@ -8,9 +8,44 @@ import javax.swing.plaf.basic.*;
 public class IconCrafter extends JFrame {
 	public static void main(String[] args) {
 		IconCrafter crafter = new IconCrafter();
-		crafter.addColor("Rød", new Color(0xED1C24));
-		crafter.addColor("Grøn", new Color(0x228B22));
-		crafter.addColor("Blå", new Color(0x007FFF));
+		int argsadded = 0;
+		boolean noparams = false;
+		for (String arg : args) {
+			if (!noparams && arg.charAt(0) == '-') {
+				if (arg.equals("--")) {
+					noparams = true;
+					continue;
+				}
+				System.err.println("Unrecognised option: "+arg);
+				continue;
+			}
+			int equals = arg.indexOf('=');
+			String name;
+			if (equals < 0) {
+				name = arg;
+			} else {
+				name = arg.substring(0, equals);
+				arg = arg.substring(equals+1);
+			}
+			int color;
+			try {
+				color = Integer.parseInt(arg, 16);
+			} catch (NumberFormatException e) {
+				System.err.println("Not a hexadecimal number: "+arg);
+				continue;
+			}
+			if (color > 0xFFFFFF || color < 0) {
+				System.err.println("Hexadecimal number out of bounds: "+arg);
+				continue;
+			}
+			crafter.addColor(name, new Color(color));
+			++argsadded;
+		}
+		if (argsadded == 0) {
+			crafter.addColor("Rød", new Color(0xED1C24));
+			crafter.addColor("Grøn", new Color(0x228B22));
+			crafter.addColor("Blå", new Color(0x007FFF));
+		}
 		crafter.pack();
 		crafter.setVisible(true);
 		crafter.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
