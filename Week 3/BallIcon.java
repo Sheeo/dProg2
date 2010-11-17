bcad2bdcbae57f2c231ae9f048470632df023e8b
Mathias Rav <rav@cs.au.dk>
2010-11-17 12:48:11 +0100

    [Week 3] Enable anti-aliasing

--- Week 3/BallIcon.java
+++ Week 3/BallIcon.java
@@ -25,10 +25,23 @@ public class BallIcon implements Icon {
 		return width;
 	}
 
+	private boolean tryEnableAntiAlias(Graphics g) {
+		Graphics2D o;
+		try {
+			o = (Graphics2D) g;
+		} catch (ClassCastException e) {
+			// wasn't a Graphics2D
+			return false;
+		}
+		o.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		return true;
+	}
+
 	/**
 	 * Draw the icon at the specified location.
 	 */
 	public void paintIcon(Component c, Graphics g, int x, int y) {
+		tryEnableAntiAlias(g);
 		g.setColor(color);
 		g.fillOval(x, y, width, height);
 	}
