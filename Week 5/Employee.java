d4c544338b3c7207e4c287cbb2f551b394581388
Mathias Rav <rav@cs.au.dk>
2010-11-30 17:26:22 +0100

    [Week 5] Spaces to tabs

--- Week 5/Employee.java
+++ Week 5/Employee.java
@@ -3,47 +3,47 @@
 */
 public class Employee
 {
-   /**
-      Constructs an Employee object
-      @param aName the employee's name
-   */
-   public Employee(String aName)
-   {
-      name = aName;
-   }
+	/**
+		Constructs an Employee object
+		@param aName the employee's name
+	*/
+	public Employee(String aName)
+	{
+		name = aName;
+	}
 
-   /**
-      Sets the salary
-      @param the new salary
-   */
-   public void setSalary(double aSalary)
-   {
-      salary = aSalary;
-   }
+	/**
+		Sets the salary
+		@param the new salary
+	*/
+	public void setSalary(double aSalary)
+	{
+		salary = aSalary;
+	}
 
-   /**
-      Returns the employee's name
-      @return the name
-   */
-   public String getName()
-   {
-      return name;
-   }
+	/**
+		Returns the employee's name
+		@return the name
+	*/
+	public String getName()
+	{
+		return name;
+	}
 
-   /**
-      Returns the employee's salary
-      @return the salary
-   */
-   public double getSalary()
-   {
-      return salary;
-   }
+	/**
+		Returns the employee's salary
+		@return the salary
+	*/
+	public double getSalary()
+	{
+		return salary;
+	}
 
-   public String toString() {
-      return getName()+", Employee has been miserable lately. He couldn't support his family with his measly salary of "+
-         salary+" lately. He was bullied at work lately.";
-   }
+	public String toString() {
+		return getName()+", Employee has been miserable lately. He couldn't support his family with his measly salary of "+
+			salary+" lately. He was bullied at work lately.";
+	}
 
-   private String name;
-   private double salary;
+	private String name;
+	private double salary;
 }
--- Week 5/Manager.java
+++ Week 5/Manager.java
@@ -3,39 +3,39 @@
 */
 public class Manager extends Employee 
 {
-   /**
-      Constructs a Manager object
-      @param aName the employee's name
-   */
-   public Manager(String aName)
-   {
-      super(aName);
-      bonus = 0;
-   }
+	/**
+		Constructs a Manager object
+		@param aName the employee's name
+	*/
+	public Manager(String aName)
+	{
+		super(aName);
+		bonus = 0;
+	}
 
-   /**
-      Sets the bonus
-      @param the new bonus
-   */
-   public void setBonus(double aBonus)
-   {
-      bonus = aBonus;
-   }
+	/**
+		Sets the bonus
+		@param the new bonus
+	*/
+	public void setBonus(double aBonus)
+	{
+		bonus = aBonus;
+	}
 
-   /**
-      Returns the manager's salary
-      @return the salary
-   */
-   public double getSalary()
-   {
-      return super.getSalary() + bonus;
-   }
+	/**
+		Returns the manager's salary
+		@return the salary
+	*/
+	public double getSalary()
+	{
+		return super.getSalary() + bonus;
+	}
 
-   public String toString() {
-      return getName()+" has been ecstatic lately. His bonus was increased to "+
-         bonus+" lately. He has earned a yearly salary of "+
-         getSalary()+" recently.";
-   }
+	public String toString() {
+		return getName()+" has been ecstatic lately. His bonus was increased to "+
+			bonus+" lately. He has earned a yearly salary of "+
+			getSalary()+" recently.";
+	}
 
-   private double bonus;
+	private double bonus;
 }
