8589c6f3f101ff26f19bdb52e9ab3c83cfc50e63
Mathias Rav <rav@cs.au.dk>
2010-12-01 13:47:47 +0100

    [Week 5] Refactor Driver

--- Week 5/Driver.java
+++ Week 5/Driver.java
@@ -3,27 +3,63 @@ public class Driver {
 	static {
 		happyDilbert.setSalary(55000);
 	}
+
 	public static void main(String[] args) {
-		Manager m = new Manager("P.H.B.");
-		m.setSalary(80000);
-		m.setBonus(10000);
+		Driver d = new Driver();
+		d.drive(args);
+	}
+
+	private Manager manager;
+	private Employee employee;
+
+	public Driver() {
+		createMananger();
+		createDilbert();
+	}
+
+	public void drive(String[] args) {
+		meetTheManager();
+		meetDilbert();
+		dilbertGetsARaise();
+		checkHappiness();
+	}
+
+	private void createMananger() {
+		manager = new Manager("P.H.B.");
+		manager.setSalary(80000);
+		manager.setBonus(10000);
+	}
+
+	private void meetTheManager() {
 		System.out.println("Meet the manager.");
-		System.out.println(m.thoughts());
-		Employee e = new Employee("Dilbert");
-		e.setSalary(44000);
+		System.out.println(manager.thoughts());
 		System.out.println();
+	}
+
+	private void createDilbert() {
+		employee = new Employee("Dilbert");
+		employee.setSalary(44000);
+	}
+
+	private void meetDilbert() {
 		System.out.println("And his employee.");
-		System.out.println(e.thoughts());
+		System.out.println(employee.thoughts());
 		System.out.println();
+	}
+
+	private void dilbertGetsARaise() {
 		System.out.println("But then, he got a raise!");
-		e.setSalary(e.getSalary()*1.25);
-		if (e.equals(happyDilbert)) {
+		employee.setSalary(employee.getSalary()*1.25);
+	}
+
+	private void checkHappiness() {
+		if (employee.equals(happyDilbert)) {
 			System.out.println("Now he is SO happy. Just look at him!");
-			System.out.println(e.thoughts());
+			System.out.println(employee.thoughts());
 		} else {
 			System.out.println("That did no good!");
 			System.out.println("Got:");
-			System.out.println(e);
+			System.out.println(employee);
 			System.out.println("Expected:");
 			System.out.println(happyDilbert);
 		}
