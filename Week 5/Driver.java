ba9abf2d5ab10c9ae142ef6d33d0393d985444dc
Mathias Rav <rav@cs.au.dk>
2010-12-03 10:12:49 +0100

    [Week 5] Revert mutability fix in CommitteeDriver

--- Week 5/Driver.java
+++ Week 5/Driver.java
@@ -57,18 +57,16 @@ class CommitteeDriver extends Driver {
 	private ArrayList<Employee> colleagues;
 	private List<Committee> committees;
 
-	/* Don't change the hashcode (directly or indirectly) of members of a
-	 * hashset after adding them to the hashset.
+	/* Mutable members are no good as members in a HashSet. Therefore, Employee
+	 * is immutable.
 	 * http://javaadventure.blogspot.com/2007/02/hashcode-pitfalls-with-hashset-and.html
-	 *
-	 * In other words, don't create the committee until after Dilbert has
-	 * gotten a raise.
 	 */
 
 	public CommitteeDriver() {
 		createMananger();
 		createDilbert();
 		createColleagues();
+		createCommittee();
 	}
 
 	public void drive(String[] args) {
@@ -76,8 +74,6 @@ class CommitteeDriver extends Driver {
 		meetDilbert();
 		dilbertGetsARaise();
 		checkHappiness();
-
-		createCommittee(); // see above on hashsets and member mutation
 		meetTheCommittee();
 	}
 
