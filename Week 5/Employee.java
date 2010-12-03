b5bb2350ee98e39f86d9ee9d59ef03bcbec9b6e8
Mathias Rav <rav@cs.au.dk>
2010-12-03 10:00:16 +0100

    [Week 5] Fix style in Employee

--- Week 5/Employee.java
+++ Week 5/Employee.java
@@ -1,44 +1,28 @@
-/**
-  A class for modeling Employees
-*/
-public class Employee
-{
-	public Employee(String aName, double aSalary)
-	{
+public class Employee {
+	private String name;
+	private double salary;
+
+	public Employee(String aName, double aSalary) {
 		name = aName;
 		salary = aSalary;
 	}
 
-	/**
-		Sets the salary
-		@param the new salary
-	*/
-	public void setSalary(double aSalary)
-	{
+	public void setSalary(double aSalary) {
 		salary = aSalary;
 	}
 
-	/**
-		Returns the employee's name
-		@return the name
-	*/
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	/**
-		Returns the employee's salary
-		@return the salary
-	*/
-	public double getSalary()
-	{
+	public double getSalary() {
 		return salary;
 	}
 
 	public String thoughts() {
-		return getName()+", Employee has been miserable lately. He couldn't support his family with his measly salary of "+
-			salary+" lately. He was bullied at work lately.";
+		return getName()+", Employee has been miserable lately. He couldn't "+
+		       "support his family with his measly salary of "+salary+
+		       " lately. He was bullied at work lately.";
 	}
 
 	public String toString() {
@@ -46,21 +30,20 @@ public class Employee
 	}
 
 	public int hashCode() {
-		return 13*super.hashCode()+17*name.hashCode()+23*new Double(salary).hashCode();
+		return 13*super.hashCode()
+		       +17*name.hashCode()
+		       +23*new Double(salary).hashCode();
 	}
 
 	public boolean equals(Object other) {
 		if (other.getClass() != Employee.class) return false;
-		//if (other.hashCode() != hashCode()) return false;
 		return employeeEquals((Employee) other);
 	}
 
 	private boolean employeeEquals(Employee other) {
 		if (!getName().equals(other.getName())) return false;
-		if (Math.abs(getSalary()-other.getSalary()) > 0.001*getSalary()) return false;
+		if (Math.abs(getSalary()-other.getSalary()) > 0.001*getSalary())
+			return false;
 		return true;
 	}
-
-	private String name;
-	private double salary;
 }
