136b92f8c3303071045b7482ff6d9b728b4c7aa1
Michael Søndergaard <msond@cs.au.dk>
2010-12-07 16:35:05 +0100

    [Week 6] Start multiset.java

---
+++ Week 6/MultiSet.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MultiSet<E> extends AbstractCollection<E>{
+	private HashMap map;
+
+	public MultiSet() {
+	
+	}
+
+	public void add(item)
+	{
+		
+	}
+	public int size() {
+		return 0;
+	}
+
+	public Iterator iterator() {
+		return null;
+	}
+}
